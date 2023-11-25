5f16e3e1d0d01a734ec7aa5d9a000895c60eaa1c
skewll <73421224+skewll@users.noreply.github.com>
2023-11-24 17:14:18 -0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import { defineNuxtConfig } from '@nuxt/bridge'
 
-const name = 'Janet Doe'
-const dates = 'July 29th 1958 - Jan 4th 2021'
+const name = 'Sean Paul Vaughn'
+const dates = 'May 17th 1986 - September 14th 2023'
 
 export default defineNuxtConfig ({
   bridge: false,
