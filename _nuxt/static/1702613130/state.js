2cc64ad87e329a98396468f6beeff11326136019
skewll <skewll@users.noreply.github.com>
2023-12-15 04:05:31 +0000

    deploy: 5a8c1f9cd8d833db9c2c036072ee6816ac3de35f

---
+++ _nuxt/static/1702613130/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1702613130",layout:"default",error:a,serverRendered:true,routePath:b,config:{name:"Sean Paul Vaughn",dates:"May 17th 1986 - September 14th 2023",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
