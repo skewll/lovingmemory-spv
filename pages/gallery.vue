937fd602f53806a8e88c69321f86e8721e425317
skewll <mcgavine@gmail.com>
2023-12-15 14:49:08 -0800

    In Development

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -26,12 +26,12 @@
         <a href="mailto:email@domain.com?subject=Upload&body=uploading">{{$config.uploadEmail}}
         </a>
       </p> -->
-      <!-- <p>*To upload to gallery, email photos or a zip file containing photos to 
-        <a :href="`mailto:${config.uploadEmail}?subject=Upload`">{{$config.uploadEmail}}
-      </p> -->
       <p>*To upload to gallery, email photos or a zip file containing photos to 
-        <a :href="'mailto:' + config.uploadEmail + '?subject=Upload'">{{$config.uploadEmail}}</a>
+        <a :href="`mailto:${{{$config.uploadEmail}}}?subject=Upload`">{{$config.uploadEmail}}</a>
       </p>
+      <!-- <p>*To upload to gallery, email photos or a zip file containing photos to 
+        <a :href="'mailto:' + config.uploadEmail + '?subject=Upload'">{{$config.uploadEmail}}</a>
+      </p>  -->
       <!-- <p>*To upload to gallery, email photos or a zip file containing photos to 
         <a :href="'mailto:' + config.uploadEmail + '?subject=Upload'">{{$config.uploadEmail}}</a>
       </p> -->
